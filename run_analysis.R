84079059d7fa97b6a6f22202b17c479f58ce4735
Jack Lee <jack.lee@hssbc.ca>
2014-04-23 22:45:45 -0700

    Update script
    Update script

---
+++ run_analysis.R
@@ -0,0 +1,22 @@
+## The script is performing:
+## 1) Merges the training and the test sets to create one data set.
+## 2) Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3) Uses descriptive activity names to name the activities in the data set
+## 4) Appropriately labels the data set with descriptive activity names.
+## 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+
+## list of the variables used in script
+## Inputs variables
+## Program internal variables
+## Output variables
+
+
+
+## Part 1) Merges the training and the test sets to create one data set.
+
+## Part 2) Extracts only the measurements on the mean and standard deviation for each measurement.
+## Part 3) Uses descriptive activity names to name the activities in the data set
+## Part 4) Appropriately labels the data set with descriptive activity names.
+## Part 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
